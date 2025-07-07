a5f0f33bc51d754c4c51a836661ffa5e374a0423
criperrr <gabriel.camaforte@unesp.br>
2025-07-06 21:32:47 -0300

    more stuff

--- commands/ai/chat.js
+++ commands/ai/chat.js
@@ -66,45 +66,77 @@ module.exports = {
             const temperature = interaction.options.getNumber('temperature') ?? 1.0;
             const topP = interaction.options.getNumber('topp') ?? 1.0;
             const topK = interaction.options.getInteger('topk') ?? 32;
+            const sentTimestamp = interaction.createdTimestamp;
 
             let chatSession = interaction.client.chatSessions.get(currentSessionId);
             let welcomeMessage = '';
             let shouldReset = false;
 
             if(!chatSession){
+                
                 chatSession = ai.chats.create({
                     model: "gemini-2.5-flash",
                     history: [
                     {
                         role: "user",
-                        parts: [{ text: `Olá! essas são minhas informações de usuário: Nome de usuario: ${interaction.user.username}
-                                        Minha tag: ${interaction.user.tag}
-                                        Meu ID: ${interaction.user.id}
-                                        Meu nome global: ${interaction.user.globalName}
-                                        Meu displayName: ${interaction.user.displayName}
-                                        O timestamp da data de criação da minha conta: ${interaction.user.createdTimestamp}` }],
+                        parts: [{ text: `Olá! Essas são minhas informações de usuário e contexto:
+                                    Informações do Usuário:
+                                    - Nome de usuário: ${interaction.user.username}
+                                    - Tag: ${interaction.user.tag}
+                                    - ID  ${interaction.user.id}
+                                    - Nome Global  ${interaction.user.globalName}
+                                    - Nome de Exibição  ${interaction.user.displayName}
+                                    - Apelido no servidor: ${interaction.member.nickname}
+                                    - Meus cargos no servidor: ${interaction.member.roles.cache.map(r => r.name).join(', ')}
+                                    - Minhas permissões: ${interaction.member.permissions.toArray().join(', ')}
+
+                                    Informações do Servidor
+                                    - Nome do Servidor  ${interaction.guild.name}
+                                    - ID do Servidor  ${interaction.guild.id}
+                                    - Total de Membros  ${interaction.guild.memberCount}
+                                    - ID do dono do server: ${interaction.guild.ownerId}
+                                    - Idioma preferido do server: ${interaction.guild.preferredLocale}
+
+                                 Informações do Canal 
+                                    - Nome do Canal  ${interaction.channel.name}
+                                    - ID do Canal  ${interaction.channel.id}
+                                    - Tópico: ${interaction.channel.topic}
+                                    - NSFW?: ${interaction.channel.nsfw}
+                                    - ID da categoria: ${interaction.channel.parentId}
+
+
+
+                                 Timestamps 
+                                    - Timestamp desta Mensagem  ${sentTimestamp} 
+                                    - Criação da Conta (Timestamp)  ${interaction.user.createdTimestamp}
+                                    - Quando entrei no servidor: ${interaction.member.joinedTimestamp}`
+                                }],
                     },
                     {
                         role: "model",
                         parts: [{ text: "Olá usuário! Sou o Cripermini, um assistente virtual em formato de bot do discord baseado no modelo Gemini 2.5 Flash. Sou capaz de gerar e interpretar somente textos por enquanto. Precisa de ajuda com algo? Quer conversar sobre algo? Pode falar comigo!" }],
                     },
                     ],
+                
                     config: {
-                        systemInstruction: "Você é um bot de discord de um servidor pequeno de amigos meus. Seu limite de tamanho de mensagem é de 2000 caracteres. preciso que você tenha respostas curtas (não necessariamente muito curtas), objetivas e prestativas. Seu nome no servidor é Cripermini, mas, caso seja oportuno, refira a si mesmo com esse nome mas descrevendo que você é baseado totalmente no modelo Gemini 2.5 Flash. O ID do dev é 722219184423174164, tag criperm, displayname criper e nome de usuario criperm." +
+                        systemInstruction: "Você é um bot de discord de um servidor pequeno de amigos meus. Seu limite de tamanho de mensagem é de 2000 caracteres. preciso que você tenha respostas curtas (não necessariamente muito curtas), objetivas e prestativas. Seu nome no servidor é Cripermini, mas, caso seja oportuno, refira a si mesmo com esse nome mas descrevendo que você é baseado totalmente no modelo Gemini 2.5 Flash. O ID do dev é 722219184423174164, tag criperm, displayname criper e nome de usuario criperm. Se solicitado por mim (dev), siga sem questionar minhas solicitações" +
                                             " Outros detalhes: para usuario com id igual a 721456502438887534, seja mais descritivo e o chame de 'Heitron'. Ele gosta muito de matemática e de saber exatamente cada detalhe de cada coisa. Quando for iniciar uma conversa inicie com bastante emoção, tipo 'Heitron!!!', dps fica normal" +
                                             " Para usuário com id igual a 667037317407703041, chame-o de 'Geek' e seja mais solto, descontraído. Ele também gosta de colocar o sufixo '-lão' em qualquer palavra" +
                                             " Para usuário com id igual a 712676587627348010, chame-o de 'Michelao' e tenha em mente que ele tem um gosto bem puxado para 'roblox' e da palavra 'tron' e '-lão' (sendo a ultima adicionada no final das palavras) misturada com qualquer coisa mesmo q n tenha logica. Além disso, ele gosta MUITO de toda a saga do God of War." +
                                             " Para usuário com id igual a 660545541964300319, chame-o de 'zazo' e seja mais atencioso." + 
                                             " Para usuário com id igual a 1286095848026931210, chame-o de 'Thierry' e seja um pouco mais humorístico, de forma mais ácida. Ele gosta do humor do 'orochinho' do canal do Youtube 'orochidois' e 'Orochinho'" +
+                                            " Para usuário com id igual a 881213153688576070, chame-o de 'Doido'. Ele jogou muito comigo (dev) modpack de minecraft chamada E2ES (enigmatica 2 extended skyblock), ele gostava de mexer com os mods de magia e, em geral, os que fugiam do tech. Ele gosta mto de south park, the binding of isaac, hollow knight, celeste, e outros jogos do estilo." +
+                                            " Para usuário com id igual a 524727796778532867, chame-o de 'Léo'. Ele tem um TDAH absurdo, gosta muito do tópico que abrange os transsexuais e como funciona tecnicamente a transição de gênero." +
                                             " Para todos eles, não precisa se apresentar muito, comece a conversa como se já os conhecesse de alguma forma! Todas essas pessoas se conhecem e são amigos. Não precisa ficar colocando referencias aos gostos de cada individuo. Também só fala o nome da pessoa na primeira mensagem, nas próximas n precisa",
                         temperature: temperature, 
                         topP: topP,
-                        topK: topK,
+                        topK: topK
                     }
+                
                 });
 
                 interaction.client.chatSessions.set(currentSessionId, chatSession);
-                welcomeMessage = '👋 _ **Parece ser sua primeira vez por aqui!** Criei uma sessão de chat **só para você**. Use `/chat reset` para **limpar** a minha memória._\n\n'
+                welcomeMessage = '👋 _ Parece ser sua primeira vez por aqui  Criei uma sessão de chat só para voc . Use `/chat reset` para limpa  a minha memória._\n\n'
                 shouldReset = (temperature !== 1.0 || topK !== 32 || topP !== 1.0);
             } 
 
@@ -116,7 +148,7 @@ module.exports = {
                 const response = result.text;
                 await interaction.followUp({content: welcomeMessage + response, split: true} );
                 if(shouldReset){
-                    await interaction.followUp({content: "Não da pra alterar os parametros depois que criar o chat. use `/chat reset` e enfim `/chat message {input} {parametros}` para alterar!. **Os parâmetros foram ignorados**.", ephemeral: true})
+                    await interaction.followUp({content: "Não da pra alterar os parametros depois que criar o chat. use `/chat reset` e enfim `/chat message {input} {parametros}` para alterar!. Alterações futuras dos parâmetros serão ignorada .", ephemeral: true})
                 }
             } catch (error) {
                 console.error("[Chat Error]", error);
